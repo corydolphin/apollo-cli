7610d4f1d4dd9f113eeed994ec465556a84033d3
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-07-19 07:39:48 -0700

    [wip] move to dependency structure

--- packages/apollo-cli/src/config.ts
+++ packages/apollo-cli/src/config.ts
@@ -1,8 +1,10 @@
-import { basename, dirname, join, relative } from "path";
+import { basename, dirname, join, relative, resolve } from "path";
 import { fs, withGlobalFS } from "apollo-codegen-core/lib/localfs";
 
 import * as fg from "glob";
 import * as minimatch from "minimatch";
+import { GraphQLSchema, buildClientSchema } from "graphql";
+import { loadSchema } from "./load-schema";
 
 export interface EndpointConfig {
   url?: string; // main HTTP endpoint
@@ -10,28 +12,46 @@ export interface EndpointConfig {
   headers?: Object; // headers to send when performing operations
 }
 
+export interface SchemaDependency {
+  schema?: string;
+  endpoint?: EndpointConfig;
+  engineKey?: string;
+}
+
+export interface DocumentSet {
+  schema?: string;
+  includes: string[];
+  excludes: string[];
+}
+
 export interface ApolloConfig {
   projectFolder: string;
   projectName?: string;
-  schema?: string; // path to JSON introspection, if not provided endpoint will be used
-  endpoint?: EndpointConfig; // GraphQL endpoint URL, used to run queries and grab schema
-  operations: string[]; // glob path(s) to GraphQL operations (default: '**/*.graphql')
-  excludedOperations?: string[]; // glob path(s) to GraphQL operation paths to ignore (default: 'node_modules/**')
+  schemas?: { [name: string]: SchemaDependency }; // path to JSON introspection, if not provided endpoint will be used
+  documents?: DocumentSet[];
   engineKey?: string; // Apollo Engine key
 }
 
-function loadEndpointConfig(obj: any, shouldDefaultURL: boolean): EndpointConfig | undefined {
+function loadEndpointConfig(
+  obj: any,
+  shouldDefaultURL: boolean
+): EndpointConfig | undefined {
   let preSubscriptions: EndpointConfig | undefined;
   if (typeof obj === "string") {
     preSubscriptions = {
       url: obj
     };
   } else {
-    preSubscriptions = (obj as EndpointConfig | undefined) ||
+    preSubscriptions =
+      (obj as EndpointConfig | undefined) ||
       (shouldDefaultURL ? { url: "http://localhost:4000/graphql" } : undefined);
   }
 
-  if (preSubscriptions && !preSubscriptions.subscriptions && preSubscriptions.url) {
+  if (
+    preSubscriptions &&
+    !preSubscriptions.subscriptions &&
+    preSubscriptions.url
+  ) {
     preSubscriptions.subscriptions = preSubscriptions.url!.replace(
       "http",
       "ws"
@@ -41,59 +61,141 @@ function loadEndpointConfig(obj: any, shouldDefaultURL: boolean): EndpointConfig
   return preSubscriptions;
 }
 
-export function loadConfig(obj: any, configDir: string, noDefaultEndpoint: boolean): ApolloConfig {
+function loadSchemaConfig(
+  obj: any,
+  defaultEndpoint: boolean
+): SchemaDependency {
   return {
-    projectFolder: configDir,
     schema: obj.schema,
-    endpoint: loadEndpointConfig(obj.endpoint, !obj.engineKey && !noDefaultEndpoint),
-    projectName: basename(configDir),
-    operations:
-      typeof obj.operations === "string"
-        ? [obj.operations]
-        : obj.operations
-          ? (obj.operations as string[])
-          : ["**/*.graphql"],
-    excludedOperations:
-      typeof obj.excludedOperations === "string"
-        ? [obj.excludedOperations]
-        : obj.excludedOperations
-          ? (obj.excludedOperations as string[])
-          : ["node_modules"],
+    endpoint: loadEndpointConfig(
+      obj.endpoint,
+      !obj.engineKey && defaultEndpoint
+    ),
     engineKey: obj.engineKey
   };
 }
 
-export function loadConfigFromFile(file: string, noDefaultEndpoint: boolean): ApolloConfig {
+function loadDocumentSet(obj: any): DocumentSet {
+  return {
+    schema: obj.schema,
+    includes:
+      typeof obj.includes === "string"
+        ? [obj.includes as string]
+        : obj.includes
+          ? (obj.includes as string[])
+          : ["**/*.graphql"],
+    excludes:
+      typeof obj.excludes === "string"
+        ? [obj.excludes as string]
+        : obj.excludes
+          ? (obj.excludes as string[])
+          : ["node_modules"]
+  };
+}
+
+export function loadConfig(
+  obj: any,
+  configDir: string,
+  defaultEndpoint: boolean
+): ApolloConfig {
+  const schemasObj = (obj.schemas || {}) as { [name: string]: any };
+  Object.keys(schemasObj).forEach(key => {
+    schemasObj[key] = loadSchemaConfig(schemasObj[key], defaultEndpoint);
+  });
+
+  if (Object.keys(schemasObj).length == 0) {
+    schemasObj["default"] = loadSchemaConfig({}, defaultEndpoint);
+  }
+
+  return {
+    projectFolder: configDir,
+    schemas: schemasObj,
+    projectName: basename(configDir),
+    documents: (obj.documents
+      ? Array.isArray(obj.documents)
+        ? (obj.documents as any[])
+        : [obj.documents]
+      : []
+    ).map(d => loadDocumentSet(d))
+  };
+}
+
+export function loadConfigFromFile(
+  file: string,
+  defaultEndpoint: boolean
+): ApolloConfig {
   if (file.endsWith(".js")) {
     delete require.cache[require.resolve(file)];
-    return loadConfig(require(file), dirname(file), noDefaultEndpoint);
+    return loadConfig(require(file), dirname(file), defaultEndpoint);
   } else if (file.endsWith("package.json")) {
     const apolloKey = JSON.parse(fs.readFileSync(file).toString()).apollo;
     if (apolloKey) {
-      return loadConfig(apolloKey, dirname(file), noDefaultEndpoint);
+      return loadConfig(apolloKey, dirname(file), defaultEndpoint);
     } else {
-      return loadConfig({}, dirname(file), noDefaultEndpoint);
+      return loadConfig({}, dirname(file), defaultEndpoint);
     }
   } else {
     throw new Error("Unsupported config file format");
   }
 }
 
-export function findAndLoadConfig(dir: string, noDefaultEndpoint: boolean): ApolloConfig {
+export function findAndLoadConfig(
+  dir: string,
+  defaultEndpoint: boolean
+): ApolloConfig {
   if (fs.existsSync(join(dir, "apollo.config.js"))) {
-    return loadConfigFromFile(join(dir, "apollo.config.js"), noDefaultEndpoint);
+    return loadConfigFromFile(join(dir, "apollo.config.js"), defaultEndpoint);
   } else if (fs.existsSync(join(dir, "package.json"))) {
-    return loadConfigFromFile(join(dir, "package.json"), noDefaultEndpoint);
+    return loadConfigFromFile(join(dir, "package.json"), defaultEndpoint);
   } else {
-    return loadConfig({}, dir, noDefaultEndpoint);
+    return loadConfig({}, dir, defaultEndpoint);
   }
 }
 
-export function getOperationPathsForConfig(config: ApolloConfig): string[] {
-  return withGlobalFS(() => {
-    return (config.operations || [])
-      .flatMap(q => fg.sync(q, { root: config.projectFolder, absolute: true }))
-      .filter(f => !(config.excludedOperations || []).some(p =>
-        minimatch(relative(config.projectFolder, f), p)));
-  });
+export interface ResolvedDocumentSet {
+  schema?: GraphQLSchema;
+  endpoint?: EndpointConfig;
+  engineKey?: string;
+
+  documentPaths: string[];
+}
+
+export async function resolveDocumentSets(
+  config: ApolloConfig,
+  needSchema: boolean
+): Promise<ResolvedDocumentSet[]> {
+  return await Promise.all(
+    (config.documents || []).map(async doc => {
+      const referredSchema = doc.schema
+        ? (config.schemas || {})[doc.schema]
+        : undefined;
+      const resolvedSchema =
+        referredSchema && needSchema && (await loadSchema(referredSchema));
+
+      return {
+        schema: resolvedSchema
+          ? buildClientSchema({ __schema: resolvedSchema })
+          : undefined,
+        endpoint: referredSchema ? referredSchema.endpoint : undefined,
+        engineKey: referredSchema ? referredSchema.engineKey : undefined,
+        documentPaths: doc.includes
+          .flatMap(i =>
+            withGlobalFS(() =>
+              fg.sync(i, { root: config.projectFolder, absolute: true })
+            )
+          )
+          .filter(
+            f =>
+              !(
+                doc.excludes.some(e =>
+                  minimatch(relative(config.projectFolder, f), e)
+                ) ||
+                (referredSchema &&
+                  referredSchema.schema &&
+                  f === resolve(config.projectFolder, referredSchema.schema))
+              )
+          )
+      };
+    })
+  );
 }
